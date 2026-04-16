d41b8356fbc4c9383e514e573ccd2375ca285c83
K.P.S. Randula <87770007+Sahani99@users.noreply.github.com>
2026-04-16 22:22:14 +0530

    fix: use environment DATABASE_URL and enable SSL for production

--- src/config/database.js
+++ src/config/database.js
@@ -5,6 +5,12 @@ dotenv.config();
 const sequelize = new Sequelize(process.env.DATABASE_URL, {
   dialect: 'postgres',
   logging: false,
+    dialectOptions: {
+    ssl: {
+      require: true,
+      rejectUnauthorized: false, // Required for Render/Supabase/Neon
+    },
+  },
 });
 console.log('Connecting to database:', process.env.DATABASE_URL);
 export default sequelize;
