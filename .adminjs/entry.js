1fe05f9f8a6d1534b75467f15d1245119e680c55
K.P.S. Randula <87770007+Sahani99@users.noreply.github.com>
2026-04-17 11:05:11 +0530

    chore: remove compiled assets to fix production dashboard

--- .adminjs/entry.js
+++
@@ -1,5 +0,0 @@
-AdminJS.UserComponents = {}
-import Dashboard from '../src/admin/components/Dashboard'
-AdminJS.UserComponents.Dashboard = Dashboard
-import SettingsList from '../src/admin/components/SettingsList'
-AdminJS.UserComponents.SettingsList = SettingsList
